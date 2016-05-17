b534b3b3e74ee35d26c32c83adb714f329af391c
Antonio Giannino <giannus@hotmail.it>
2016-05-17 23:22:28 +0200

    Unit 8

--- Calendar.py
+++ Calendar.py
@@ -1,4 +1,8 @@
 """
+TITLE: Calendar.py
+UNIT: 8
+AUTHOR: Antonio Giannino
+
 So far, you've used Python to build a variety of things, including calculators and games. In this project, we'll build a basic calendar that the user will be able to interact with from the command line. The user should be able to choose to:
 
 - View the calendar
@@ -12,7 +16,6 @@ The program should behave in the following way:
 2. Prompt the user to view, add, update, or delete an event on the calendar
 3. Depending on the user's input: view, add, update, or delete an event on the calendar
 4. The program should never terminate unless the user decides to exit
-
 """
 
 from time import sleep, strftime
